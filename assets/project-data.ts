c0409c1a81ffaba9d6897078ab887c889f960213
Jae Min Seo <jmseo8441@gmail.com>
2023-09-12 00:35:45 +1200

    Auto-generated commit [ci skip]

---
+++ assets/project-data.ts
@@ -0,0 +1,52 @@
+import { ProjectInformation } from "./interfaces/project-interface"
+
+export const projectData: ProjectInformation[] = [
+    {
+        titleText: "BIOMENG 241 Colorimeter", 
+        imagePath: "./cat.jpeg",
+        shortDescription: "A color detection prototype made using Altium Designer, Autodesk Inventor, and programmed with LabVIEW.",
+        longDescription: "blah blah blah"
+    },
+    {
+        titleText: "BIOMENG 241 Colorimeter", 
+        imagePath: "./cat.jpeg",
+        shortDescription: "A color detection prototype made using Altium Designer, Autodesk Inventor, and programmed with LabVIEW.",
+        longDescription: "blah blah blah"
+    },
+    {
+        titleText: "BIOMENG 241 Colorimeter", 
+        imagePath: "./cat.jpeg",
+        shortDescription: "A color detection prototype made using Altium Designer, Autodesk Inventor, and programmed with LabVIEW.",
+        longDescription: "blah blah blah"
+    },
+    {
+        titleText: "BIOMENG 241 Colorimeter", 
+        imagePath: "./cat.jpeg",
+        shortDescription: "A color detection prototype made using Altium Designer, Autodesk Inventor, and programmed with LabVIEW.",
+        longDescription: "blah blah blah"
+    },
+    {
+        titleText: "BIOMENG 241 Colorimeter", 
+        imagePath: "./cat.jpeg",
+        shortDescription: "A color detection prototype made using Altium Designer, Autodesk Inventor, and programmed with LabVIEW.",
+        longDescription: "blah blah blah"
+    },
+    {
+        titleText: "BIOMENG 241 Colorimeter", 
+        imagePath: "./cat.jpeg",
+        shortDescription: "A color detection prototype made using Altium Designer, Autodesk Inventor, and programmed with LabVIEW.",
+        longDescription: "blah blah blah"
+    },
+    {
+        titleText: "BIOMENG 241 Colorimeter", 
+        imagePath: "./cat.jpeg",
+        shortDescription: "A color detection prototype made using Altium Designer, Autodesk Inventor, and programmed with LabVIEW.",
+        longDescription: "blah blah blah"
+    },
+    {
+        titleText: "BIOMENG 241 Colorimeter", 
+        imagePath: "./cat.jpeg",
+        shortDescription: "A color detection prototype made using Altium Designer, Autodesk Inventor, and programmed with LabVIEW.",
+        longDescription: "blah blah blah"
+    },
+]
